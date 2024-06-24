1cf16ad07979612c1ed6a84b7f4e7f3d2e7185b6
QTrung21 <doublet2k3@gmail.com>
2024-06-24 21:53:41 +0700

    Init project

---
+++ src/Java_core/HelloWorld.java
@@ -0,0 +1,10 @@
+package Java_core;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world with Java");
+		System.out.println("Hello world with Java");
+		System.out.println("Hello world with Java");
+		System.out.println("Hello world with Java");
+	}
+}
